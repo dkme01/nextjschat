2ab7cffa7a55e5a7e19cc69a543284153bebef68
dkme01 <dkme01@gmail.com>
2024-03-05 18:52:00 -0300

    Users page

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,10 +1,15 @@
 import {ReactNode} from "react";
 import Sidebar from "@/app/components/sidebar/Sidebar";
+import getUsers from "@/app/actions/getUsers";
+import UserList from "@/app/users/components/UserList";
 
 export default async function UsersLayout({children}: { children: ReactNode }) {
+	const users = await getUsers();
+	
 	return (
 		<Sidebar>
 			<div className="h-full">
+				<UserList users={users}/>
 				{children}
 			</div>
 		</Sidebar>
