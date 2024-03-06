37f082833f395ad680c5d99cb740d861d041c048
dkme01 <dkme01@gmail.com>
2024-03-06 06:37:08 -0300

    Fixes

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 
 import prisma from "@/app/libs/prismadb"
 
-export const authOptions: AuthOptions = {
+const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -10,7 +10,6 @@ export default async function UsersLayout({
   const users = await getUsers();
 
   return (
-    // @ts-expect-error Server Component
     <Sidebar>
       <div className="h-full">
         <UserList items={users} />
